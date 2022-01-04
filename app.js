b53f75b691cd33aa6d2dafcaf83051e8688221d0
Mayki Hu <maykihu8@gmail.com>
2022-01-03 18:04:55 -0800

    Module 7 completed: migrated to mongodb (not sql)

--- app.js
+++ app.js
@@ -2,28 +2,11 @@ const express = require('express');
 const chalk = require('chalk');
 const morgan = require('morgan');
 const path = require('path');
-const sql = require('mssql');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const config = {
-  user: 'library',
-  password: '',
-  server: 'pslibrary.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
-  database: 'PSLibrary',
-
-  options: {
-    encrypt: true, // Use this if you're on Windows Azure
-  },
-};
-
-sql.connect(config).catch(err => console.log(err)); // eslint-disable-line
 app.use(morgan('tiny'));
-
-app.use((req, res, next) => {
-  next();
-});
 
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
@@ -34,12 +17,14 @@ app.set('view engine', 'ejs');
 
 const nav = [
   { link: '/books', title: 'Book' },
-  { link: '/authors', title: 'Author' }
+  { link: '/authors', title: 'Author' },
 ];
 
 const bookRouter = require('./src/routes/bookRoutes')(nav);
+const adminRouter = require('./src/routes/adminRoutes')(nav);
 
 app.use('/books', bookRouter);
+app.use('/admin', adminRouter);
 app.get('/', (req, res) => {
   res.render(
     'index',
